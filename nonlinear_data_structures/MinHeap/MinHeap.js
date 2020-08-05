212a7a8706978760d99ec2b8afaa67517a727a6b
Sarah Purvis <onplanetoff@darwin.attlocal.net>
2020-08-05 15:39:56 -0700

    adds logger parameter & printHeap method

--- nonlinear_data_structures/MinHeap/MinHeap.js
+++ nonlinear_data_structures/MinHeap/MinHeap.js
@@ -1,59 +1,74 @@
 class MinHeap {
-    constructor() {
+    constructor(logger = null) {
         this.heap = [null];
         this.size = 0;
+        this.logger = logger;
+    }
+
+    add(value) {
+        this.heap.push(value);
+        if(this.logger !== null) {
+            this.logger.log(`Adding ${value}`);
+        }
+        this.size++;
+        this._bubbleUp();
+        if(this.logger !== null) {
+            this.logger.log(`New heap: [${this.heap}]`);
+        }
     }
 
     popMin() {
         if (this.size === 0) {
             return null;
         }
-        console.log(`${this.heap[1]} is swapped with ${this.heap[this.size]}`)
-        this.swap(1, this.size);
+        if(this.logger !== null) {
+            this.logger.log(`${this.heap[1]} is swapped with ${this.heap[this.size]}`);
+        }
+        this._swap(1, this.size);
         const min = this.heap.pop();
         this.size--;
-        console.log(`\nRemoved ${min}: [${this.heap}]`);
-        this.heapify();
-        console.log(`New heap: [${this.heap}]`);
+        if(this.logger !== null) {
+            this.logger.log(`Removed ${min}: [${this.heap}]`);
+        }
+        this._heapify();
+        if(this.logger !== null) {
+            this.logger.log(`New heap: [${this.heap}]`);
+        }
         return min;
     }
     
-    add(value) {
-        this.heap.push(value);
-        console.log(`\nAdding ${value}`);
-        this.size++;
-        this.bubbleUp();
-        console.log(`New heap: [${this.heap}]`);
+    printHeap() {
+        console.log(this.heap);
     }
 
-    bubbleUp() {
+    _bubbleUp() {
         let currentIndex = this.size;
         let currentNum = this.heap[this.size];
         
         while (currentIndex > 1 && currentNum < this.heap[getParentIndex(currentIndex)]) {
             
-            this.swap(currentIndex, getParentIndex(currentIndex));
+            this._swap(currentIndex, getParentIndex(currentIndex));
             
             currentIndex = getParentIndex(currentIndex);
         }
     }
 
-    heapify() {
+    _heapify() {
         let currentIndex = 1;
         let leftChildIndex = getLeftChildIndex(currentIndex);
         let rightChildIndex = getRightChildIndex(currentIndex);
 
-        while (this.canSwap(currentIndex, leftChildIndex, rightChildIndex)) {
-            if (this.exists(leftChildIndex) && this.exists(rightChildIndex)){
+        while (this._canSwap(currentIndex, leftChildIndex, rightChildIndex)) {
+            if (this._exists(leftChildIndex) && this._exists(rightChildIndex)){
                 if (this.heap[leftChildIndex] < this.heap[rightChildIndex]) {
-                    this.swap(currentIndex, leftChildIndex);
+                    this._swap(currentIndex, leftChildIndex);
                     currentIndex = leftChildIndex;
                 } else {
-                    this.swap(currentIndex, rightChildIndex);
+                    this._swap(currentIndex, rightChildIndex);
                     currentIndex = rightChildIndex;
                 }
             } else {
-                this.swap(currentIndex, leftChildIndex);
+                this._swap(currentIndex, leftChildIndex);
                 currentIndex = leftChildIndex;
             }
 
@@ -62,18 +77,18 @@ class MinHeap {
         }
     }
 
-    exists(index) {
+    _exists(index) {
         return index <= this.size;
     }
 
-    canSwap(currentIndex, leftChildIndex, rightChildIndex) {
+    _canSwap(currentIndex, leftChildIndex, rightChildIndex) {
         return (
-            this.exists(leftChildIndex) && this.heap[currentIndex] > this.heap[leftChildIndex]
-            || this.exists(rightChildIndex) && this.heap[currentIndex] > this.heap[rightChildIndex]
+            this._exists(leftChildIndex) && this.heap[currentIndex] > this.heap[leftChildIndex]
+            || this._exists(rightChildIndex) && this.heap[currentIndex] > this.heap[rightChildIndex]
         );
     }
 
-    swap(x, y) {
+    _swap(x, y) {
         [this.heap[x], this.heap[y]] = [this.heap[y], this.heap[x]];
     }
 }
@@ -83,7 +98,7 @@ const getLeftChildIndex = index => index * 2;
 const getRightChildIndex = index => index * 2 + 1;
 
 
-const minHeap = new MinHeap();
+const minHeap = new MinHeap(console);
 
 // helper function to return a random integer
 function randomize() { return Math.floor(Math.random() * 40); }
@@ -94,5 +109,9 @@ for (let i=0; i < 6; i++) {
   minHeap.add(randomize());
 }
 
+minHeap.printHeap();
+
 // remove min
 minHeap.popMin();
+
+minHeap.printHeap();
