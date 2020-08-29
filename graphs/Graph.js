4a061fd73a673f71cf041cb7951214afce036d53
Sarah Purvis <onplanetoff@darwin.attlocal.net>
2020-08-28 18:08:09 -0700

    adding dijkstras method to Graph class

--- graphs/Graph.js
+++ graphs/Graph.js
@@ -1,4 +1,5 @@
 const Queue = require('../linear_data_structures/Queue/Queue');
+const PriorityQueue = require('../nonlinear_data_structures/PriorityQueue/PriorityQueue');
 
 class Graph {
     constructor(isDirected = false, isWeighted = false) {
@@ -89,6 +90,36 @@ class Graph {
 
     printVertices() {
         this.vertices.forEach(vertex => vertex.print());
+    }
+
+    dijkstras(startingVertex) {
+        const distances = {};
+        const previous = {};
+        const queue = new PriorityQueue();
+        queue.add({ data: startingVertex, priority: 0 });
+    
+        this.vertices.forEach(vertex => {
+            distances[vertex.data] = Infinity;
+            previous[vertex.data] = null;
+        });
+        distances[startingVertex.data] = 0;
+    
+        while (!queue.isEmpty()) {
+            const vertex = queue.popMin().data;
+    
+            vertex.edges.forEach(edge => {
+                const alternate = edge.weight + distances[vertex.data];
+                const neighbor = edge.end;
+                const neighborData = edge.end.data;
+    
+                if (alternate < distances[neighborData]) {
+                    distances[neighborData] = alternate;
+                    previous[neighborData] = vertex;
+                    queue.add({data: neighbor, priority: distances[neighborData]});
+                }
+            })
+        }
+        return { distances, previous };
     }
 }
 
