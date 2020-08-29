c291dd3ab52660160fdffb3f6f2d13c875671bc0
Sarah Purvis <onplanetoff@darwin.attlocal.net>
2020-08-28 18:08:37 -0700

    adding test for dijkstras method on Graph class

--- graphs/Graph.spec.js
+++ graphs/Graph.spec.js
@@ -32,7 +32,7 @@ describe('Class: Graph', () => {
         });
     });
 
-    describe('Traversals', () => {
+    describe('Non-weighted traversals', () => {
         const graph = new Graph();
         const vertices =['a', 'b', 'c', 'd', 'e', 'f'];
         const edges = [['a', 'b'], ['a', 'c'], ['b', 'd'], ['d', 'e'], ['c', 'f']];
@@ -62,6 +62,43 @@ describe('Class: Graph', () => {
                 });
     
                 expect(resultArr).toStrictEqual(expected);
+            });
+        });
+    });
+
+    describe('Weighted traversals', () => {
+        const graph = new Graph(true, true);
+        const a = graph.addVertex('A');
+        const b = graph.addVertex('B');
+        const c = graph.addVertex('C');
+        const d = graph.addVertex('D');
+        const e = graph.addVertex('E');
+        const f = graph.addVertex('F');
+        const g = graph.addVertex('G');
+
+        graph.addEdge(a, c, 100);
+        graph.addEdge(a, b, 3);
+        graph.addEdge(a, d, 4);
+        graph.addEdge(d, c, 3);
+        graph.addEdge(d, e, 8);
+        graph.addEdge(e, b, -2);
+        graph.addEdge(e, f, 10);
+        graph.addEdge(b, g, 9);
+        graph.addEdge(e, g, -50);
+
+        describe('Method: dijkstras', () => {
+            it('Should return shortest distances & previously visited vertices', () => {
+                const expectedDistances = { A: 0, B: 3, C: 7, D: 4, E: 12, F: 22, G: -38 };
+                const expectedPrevious = { A: null,
+                    B: a,
+                    C: d,
+                    D: a,
+                    E: d,
+                    F: e,
+                    G: e };
+                const results = graph.dijkstras(graph.vertices[0]);
+                expect(results.distances).toStrictEqual(expectedDistances);
+                expect(results.previous).toStrictEqual(expectedPrevious);
             });
         });
     });
