fd7ae839e17367244cda46d21c853d20c6ee710b
Sarah Purvis <onplanetoff@darwin.attlocal.net>
2020-09-21 13:29:52 -0700

    adding optional logger & type error to bubbleSort

--- sorting_algorithms/bubbleSort.js
+++ sorting_algorithms/bubbleSort.js
@@ -1,10 +1,15 @@
-const bubbleSort = arr => {
+const bubbleSort = (arr, log = false) => {
+    if (!(arr instanceof Array)) {
+        throw new Error('Expected array');
+    }
     let swapping = true;
     while (swapping) {
         swapping = false;
         for (let i = 0; i < arr.length - 1; i++) {
             if (arr[i] > arr[i + 1]) {
-                console.log(`Swapping pair ${arr[i]}, ${arr[i + 1]} in [${arr}]`);
+                if (log) {
+                    console.log(`Swapping pair ${arr[i]}, ${arr[i + 1]} in [${arr}]`);
+                }
                 swap(arr, i, i + 1);
                 swapping = true;
             }
@@ -16,7 +21,5 @@ const bubbleSort = arr => {
 const swap = (arr, index1, index2) => {
     [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
 }
-
-console.log(bubbleSort([9, 8, 7, 6, 5, 4, 3, 2, 1]));
 
 module.exports = bubbleSort;
