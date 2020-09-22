78be1d62279105a965397938108381b4c2f7b4f8
Sarah Purvis <onplanetoff@darwin.attlocal.net>
2020-09-22 13:21:04 -0700

    adding tests for mergeSort function

--- sorting_algorithms/sortingAlgorithms.spec.js
+++ sorting_algorithms/sortingAlgorithms.spec.js
@@ -1,4 +1,8 @@
 const bubbleSort = require('./bubbleSort');
+const mergeSort = require('./mergeSort');
+
+const unsorted = [2, -1, 5, 2, 0, -2];
+const sorted = [-2, -1, 0, 2, 2, 5];
 
 describe('Function: bubbleSort', () => {
     it('throws an error if not given an array', () => {
@@ -7,12 +11,21 @@ describe('Function: bubbleSort', () => {
           }).toThrow();
     });
     it('correctly sorts an array', () => {
-        const unsorted = [2, -1, 0, -2];
-        const sorted = [-2, -1, 0, 2];
-
         const result = bubbleSort(unsorted);
 
         expect(result).toStrictEqual(sorted);
     });
 });
 
+describe('Function: mergeSort', () => {
+    it('throws an error if not given an array', () => {
+        expect(() => {
+            mergeSort('not an array');
+          }).toThrow();
+    });
+    it('correctly sorts an array', () => {
+        const result = mergeSort(unsorted);
+
+        expect(result).toStrictEqual(sorted);
+    });
+});
