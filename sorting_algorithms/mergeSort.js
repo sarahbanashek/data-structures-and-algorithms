19267ef465fbf1cafd2c1ba325667ed4e1815bdd
Sarah Purvis <onplanetoff@darwin.attlocal.net>
2020-09-22 13:21:53 -0700

    adding optional logger parameter to mergeSort

--- sorting_algorithms/mergeSort.js
+++ sorting_algorithms/mergeSort.js
@@ -1,30 +1,32 @@
-const mergeSort = arr => {
+const mergeSort = (arr, log = false) => {
+    if (!(arr instanceof Array)) {
+        throw new Error('Expected array');
+    }
     const length = arr.length;
     if (length === 1) {
-        console.log(arr);
         return arr;
     }
     const midpoint = Math.floor(length / 2);
     const leftArr = arr.slice(0, midpoint);
     const rightArr = arr.slice(midpoint);
-    return merge(mergeSort(leftArr), mergeSort(rightArr));
+    return merge(mergeSort(leftArr), mergeSort(rightArr), log);
 }
 
-const merge = (leftArr, rightArr) => {
+const merge = (leftArr, rightArr, log) => {
     const sortedArr = [];
     while (leftArr.length > 0 && rightArr.length > 0) {
-        console.log(`left arr: [${leftArr}]     right arr: [${rightArr}]`)
+        if (log) {
+            console.log(`left arr: [${leftArr}] \nright arr: [${rightArr}]`);
+        }
         leftArr[0] < rightArr[0] ?
             sortedArr.push(leftArr.shift()) :
             sortedArr.push(rightArr.shift());
-        console.log(`merging: [${sortedArr}]`);
+        if (log) {
+            console.log(`merging: [${sortedArr}]\n`);
+        }
     }
     return sortedArr.concat(leftArr).concat(rightArr);
 }
-
-const inputArr = [3, 5, 2, 90, 4, 7];
-
-console.log(mergeSort(inputArr));
 
 
 module.exports = mergeSort;
