ecfa590eca437cb2b136b86f9eb8bc1619a548ca
Sarah Purvis <onplanetoff@darwin.attlocal.net>
2020-10-02 14:16:12 -0700

    moving graph folder to non_linear_data structures folder

--- nonlinear_data_structures/graphs/Graph.js
+++ nonlinear_data_structures/graphs/Graph.js
@@ -3,8 +3,8 @@ TODO:
 - add method to list the nighbors of a vertex
 - add method to change the value associated with a vertex
 */
-const Queue = require('../linear_data_structures/Queue/Queue');
-const PriorityQueue = require('../nonlinear_data_structures/PriorityQueue/PriorityQueue');
+const Queue = require('../../linear_data_structures/Queue/Queue');
+const PriorityQueue = require('../PriorityQueue/PriorityQueue');
 
 class Graph {
     constructor(isDirected = false, isWeighted = false) {
